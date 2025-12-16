836591908203afe56678c5d348f0adf8d592cedb
caalami123 <caalamiagency@gmail.com>
2025-12-16 23:25:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-const RPC = "http://18.117.220.138:8545"; // AWS Public RPC
-async function rpc(method, params = []) {
-    const res = await fetch(RPC, {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            jsonrpc: "2.0",
-            id: 1,
-            method,
-            params
-        })
-    });
 
-    return res.json();
-}
